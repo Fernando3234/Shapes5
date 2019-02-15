9ae2afd8b7a3734764ab190640c4c57e4573a843
csPython <cspython@AD25TF04NGF1J-33.local>
2019-02-15 13:32:52 -0500

    added second shape to another corner

--- shapes5FA.py
+++ shapes5FA.py
@@ -2,15 +2,18 @@
 from graphics import *
 
 #setting window size and window coordinates
-rTriWin =GraphWin("Multiple Shapes", 800, 800)
-rTriWin.setCoords(0, 0, 800, 800)
+shapesWin =GraphWin("Multiple Shapes", 800, 800)
+shapesWin.setCoords(0, 0, 800, 800)
 
-#First object         
-rTri = Polygon(Point(10, 790), Point(10, 700), Point (200, 700))
+#First object          
+rTri = Polygon(Point(1, 790), Point(10, 700), Point (200, 700))
 rTri.setFill(color_rgb(220,30,20))
-rTri.draw(rTriWin)
-                                                                     
-rTriWin.getmouse()
-rTriWin.close()
+rTri.draw(shapesWin)
 
-                 
+#Second object
+rRectangle = Polygon(Point(790, 10), Point(700, 10), Point (700, 200), Point (790, 200))
+rRectangle.setFill(color_rgb(220,30,20))
+rRectangle.draw(shapesWin)
+
+shapesWin.getMouse()
+shapesWin.close()
